143832803f3df5df08f9817387d65e1fe2910c73
yangdaweihit <yangdw@hit.edu.cn>
2017-09-25 16:18:43 +0800

    edit gitignore

--- auto/symbols.el
+++
@@ -1,7 +0,0 @@
-(TeX-add-style-hook
- "symbols"
- (lambda ()
-   (TeX-add-symbols
-    '("mafan" 3)
-    '("integralx" 3))))
-
--- auto/test.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook
- "test"
- (lambda ()
-   (TeX-run-style-hooks
-    "latex2e"
-    "article"
-    "art10")))
-
